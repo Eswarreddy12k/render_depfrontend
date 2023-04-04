9318c89d3103fb6f4694caa165c8a28b92cb3f96
Eswar Reddy <eswarreddy7999@gmail.com>
2023-04-04 20:37:16 +0530

    ver2f1

--- index.js
+++ index.js
@@ -27,8 +27,9 @@ app.use(jsonParser);
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, "..", "build")));
 app.use(express.static("public"));
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+app.use(function (request, response, next) {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
